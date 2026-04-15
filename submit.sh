7b0c8ec3cf1eb35cd356a799078d11476c9dd08f
Penguin-rgb-dev <divesh20243414@students.iisertirupati.ac.in>
2026-04-15 17:14:10 +0530

    my first slurm script

---
+++ submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=RMP_Linear_Scan
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=64          # This value is what Python will now see
+#SBATCH --partition=short
+#SBATCH --time=04:00:00
+#SBATCH --mem=128G
+
+# 1. Load your Conda environment
+module load miniconda3-2025.11.1
+source activate quantum_env
+
+# 2. Run the script 
+# No 'export' needed here because you updated the .py file!
+python RMP_LinearMemory_C_vs_h.py
